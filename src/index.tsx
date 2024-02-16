42fead53799d0ca3f1c01f119118d7ba7af27772
Arthur Augusto <arthur.augusto.sn@gmail.com>
2024-02-16 17:20:42 -0300

    inserting tailwind on global.css

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./app";
+import App from "./app/app";
 import reportWebVitals from "./reportWebVitals";
+import "./global.css";
 
 ReactDOM.render(
   <React.StrictMode>
